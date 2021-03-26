b7a818949772dccf732e366da41f8cb88780ed45
Diego Figueiredo <diegosf_dsf@hotmail.com>
2021-03-26 10:18:25 -0300

    c0 - Iniciando versionamento

---
+++ src/Models/ServicoModel.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author CASA
+ */
+public class ServicoModel {
+    private int ser_codigo;
+    private String ser_descricao;
+}
